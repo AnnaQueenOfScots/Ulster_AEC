c225d36adb110630fcc16754c4227c24dcd6f750
AnnaQueenOfScots <cuninghame-a@ulster.ac.uk>
2022-05-03 13:23:16 +0100

    adding other spatial files
    trying to solve fiona error

--- script.py
+++ script.py
@@ -8,7 +8,12 @@ import cartopy.crs as ccrs
 import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
 
-
+flood_5m = gpd.read_file('data_files/5m_flood.shp')
+hist_flood = gpd.read_file('data_files/historical_flooding.shp')
+boundary = gpd.read_file('data_files/Fermanagh_DCA.shp')
+l_erne = gpd.read_file('data_files/l_erne.shp')
+buildings = gpd.read_file('data_files/Pointer_Fermanagh.shp')
+lc = gpd.read_file('data_files/LCM_Fermanagh.shp')
 
 
 #fig.savefig('Fermanagh.png', dpi=300, bbox_inches='tight')
