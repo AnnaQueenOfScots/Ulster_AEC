488b4b8fee2f6b476728071e84d3a0b33de4fcce
AnnaQueenOfScots <cuninghame-a@ulster.ac.uk>
2022-05-03 10:58:53 +0100

    Add .yml file
    Cloned EGM722 repository and used CMD prompt to export the .yml

---
+++ script.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point, LineString, Polygon
+import os
+import matplotlib.pyplot as plt
+from cartopy.feature import ShapelyFeature
+import cartopy.crs as ccrs
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
