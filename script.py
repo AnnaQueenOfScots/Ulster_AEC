0c4d9d37361d0d87b115d54b714b853199a1ce9c
AnnaQueenOfScots <cuninghame-a@ulster.ac.uk>
2022-05-06 21:13:03 +0100

    worked on code

--- script.py
+++ script.py
@@ -10,38 +10,33 @@ from cartopy.feature import ShapelyFeature
 import cartopy.crs as ccrs
 
 desired_width=320
-
 pd.set_option('display.width', desired_width)
-
-np.set_printoptions(linewidth=desired_width)
-
-pd.set_option('display.max_columns',40) # this tells PyCharm to show you the whole head of a table, not just 5 columns
+np.set_printoptions(linewidth = desired_width)
+pd.set_option('display.max_columns',40) # this block of code modified from Stack Overflow tells PyCharm to show you the whole head of a table, not just 5 columns
 
 
 flood_5m = gpd.read_file('data_files/flood_5m.shp')
-hist_flood = gpd.read_file('data_files/historical_flooding.shp')
+hist_flood = gpd.read_file('data_files/hist_flood.shp')
 boundary = gpd.read_file('data_files/Fermanagh_DCA.shp')
 l_erne = gpd.read_file('data_files/ll_erne.shp')
 buildings = gpd.read_file('data_files/Pointer_Fermanagh.shp')
-lc = gpd.read_file('data_files/LCM_Fermanagh.shp')
+lc = gpd.read_file('data_files/LCM_Fermanagh.shp') # read the data files
 
 flood_5m.to_crs(epsg = 2157)
 hist_flood.to_crs(epsg = 2157)
 boundary.to_crs(epsg = 2157)
 l_erne.to_crs(epsg = 2157)
 buildings.to_crs(epsg = 2157)
-lc.to_crs(epsg = 2157)
+lc.to_crs(epsg = 2157) # convert files to Irish Transverse Mercator
 
-myFig = plt.figure(figsize=(12, 12))  # create a figure of size 10x10 (representing the page size in inches)
+"""
+myFig = plt.figure(figsize=(12, 12))  # create a figure of size 12x12 inches
 
-myCRS = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system to transform our data.
-# be sure to fill in XX above with the correct number for the area we're working in.
+myCRS = ccrs.UTM(29)  # transform data to appropriate section of the Universal Transverse Mercator
 
-ax = plt.axes(projection=ccrs.Mercator())  # finally, create an axes object in the figure, using a Mercator
-# projection, where we can actually plot our data.
+ax = plt.axes(projection=ccrs.Mercator())  # create an object with axes in the figure with Mercator projection to plot map data
 
-
-outline_feature = ShapelyFeature(boundary['geometry'], myCRS, edgecolor='black', facecolor='white')
+outline_feature = ShapelyFeature(boundary['geometry'], myCRS, edgecolor='black', facecolor='white') # plot the county outline
 xmin, ymin, xmax, ymax = boundary.total_bounds # tells matplotlib to use boundary's total bounds
 ax.add_feature(outline_feature) # adds boundary to map
 ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS) # sets map extent to extent of county boundary
@@ -52,46 +47,51 @@ print('No. lc classes: {}'.format(num_lc)) # counts how many unique land covers 
 list_lc = list(lc.LAND_COVER.unique())
 print(list_lc) # prints a list of unique land covers
 
-lc_colours = ['aqua','lightgreen','lawngreen','yellowgreen','sienna','lime','forestgreen','lime','olivedrab','darkgreen','saddlebrown','dimgray','olive','gray','lightgray','darkblue']
+lc_colours = ['aqua','lightgreen','lawngreen','yellowgreen','sienna','lime','forestgreen','lime','olivedrab','darkgreen','saddlebrown','dimgray','olive','gray','lightgray','darkblue'] #list of colours for land cover in order of those listed in output by above step
 
 for i, name in enumerate(list_lc):
-    lc_feat = ShapelyFeature(lc['geometry'][lc['LAND_COVER'] == name], myCRS,
-                          edgecolor='black',
-                          facecolor=lc_colours[i],
-                          linewidth=0,
-                          alpha=0.75)
-    ax.add_feature(lc_feat)
+    lc_feat = ShapelyFeature(lc['geometry'][lc['LAND_COVER'] == name], myCRS, # iterate over and assign a new colour to each of the land cover classes and create land cover feature object
+                          edgecolor='black', # plot outline in black
+                          facecolor=lc_colours[i], # plot the land covers in their unique colours assigned above
+                          linewidth=0, # don't put outlines on land cover polygons
+                          alpha=0.75) # colour saturation
+    ax.add_feature(lc_feat) # plot the above on the axis
 
-flood_5m_feat = ShapelyFeature(flood_5m['geometry'], myCRS,
-                            edgecolor='black',
-                            facecolor = 'red',
-                            hatch = 'xx',
-                            linewidth=0.5)
-ax.add_feature(flood_5m_feat)
+flood_5m_feat = ShapelyFeature(flood_5m['geometry'], myCRS, # create feature object of a 5m flood event of Lower Lough Erne
+                            edgecolor='black', # plot outline in black
+                            facecolor = 'red', # plot the flood in red
+                            hatch = 'xx', # with a hatch to distinguish from land cover
+                            linewidth=0.5) # width of bounding lines
+ax.add_feature(flood_5m_feat) # plot the above on the axis
 
-l_erne_feat = ShapelyFeature(l_erne['geometry'], myCRS,
-                            edgecolor='royalblue',
-                            facecolor = 'royalblue',
-                            linewidth=0.2)
-ax.add_feature(l_erne_feat)
-plt.savefig('Plot.png')
+l_erne_feat = ShapelyFeature(l_erne['geometry'], myCRS, # create a feature object of Lower Lough Erne
+                            edgecolor='royalblue', # plot outline in royal blue
+                            facecolor = 'royalblue',  # plot Lough Erne in royal blue
+                            linewidth=0.2) # widh of bounding lines
+ax.add_feature(l_erne_feat) # plot the above on the axis
+
+plt.savefig('Plot.png') # save the plotted figure
 
 
-print(l_erne.head())
-print(l_erne.TEMA.unique())
 
+area_flooded_5m = sum((flood_5m.area)-(sum(l_erne.area))) # the area of land that will be flooded in Lower Lough Erne experiences a 5m flood
+area_flooded_hist = sum((hist_flood.area)-(sum(l_erne.area))) # the area of land that was flooded in Lower Lough Erne historical flood event
+
+print('Land inundated during 5m flood: {}'.format(area_flooded_5m)) #print the above value
+print('Land inundated during historical flood event: {}'.format(area_flooded_hist)) # print the above value
 """
-print((sum(flood_5m.area))-(sum(l_erne.area)))
+
+flood_geom = flood_5m['geometry'].values[0] # select flood polygon
+is_flooded = buildings['geometry'].within(flood_geom) # select all buildings within fLood polygon
+
+count_buildings = buildings[is_flooded]['BUILDING_S'].size # count
+print(count_buildings)
+
+#print(subset.groupby['TOWN'])
 
 joined = gpd.sjoin(flood_5m, buildings, how = 'inner', lsuffix='left', rsuffix='right')
-print(joined.head())
+#print(joined.head())
 
 subset = gpd.sjoin(flood_5m, buildings, how='inner')
 
-flood_geom = flood_5m['geometry'].values[0]
-is_flooded = buildings['geometry'].within(flood_geom)
 
-subset = buildings[is_flooded]
-
-print(subset.groupby['TOWN'])
-"""
