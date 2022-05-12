a82ec917233eb6b5e73a34e0f6cc533627ed5d37
AnnaQueenOfScots <cuninghame-a@ulster.ac.uk>
2022-05-12 07:20:14 +0100

    Added roads shapefile

--- script.py
+++ script.py
@@ -21,6 +21,7 @@ boundary = gpd.read_file('data_files/Fermanagh_DCA.shp')
 l_erne = gpd.read_file('data_files/ll_erne.shp')
 buildings = gpd.read_file('data_files/Pointer_Fermanagh.shp')
 lc = gpd.read_file('data_files/LCM_Fermanagh.shp') # read the data files
+roads = gpd.read_file('data_files/Fermanagh roads.shp')
 
 flood_5m.to_crs(epsg = 2157)
 hist_flood.to_crs(epsg = 2157)
@@ -29,15 +30,15 @@ l_erne.to_crs(epsg = 2157)
 buildings.to_crs(epsg = 2157)
 lc.to_crs(epsg = 2157) # convert files to Irish Transverse Mercator
 
-
+"""
 myFig = plt.figure(figsize=(12, 12))  # create a figure of size 12x12 inches
 
-myCRS = ccrs.UTM(29)  # transform data to appropriate section of the Universal Transverse Mercator
+myCRS = ccrs.UTM(29) # transform data to Ireland section of Universal Transverse Mercator 
 
 ax = plt.axes(projection=ccrs.Mercator())  # create an object with axes in the figure with Mercator projection to plot map data
 
 outline_feature = ShapelyFeature(boundary['geometry'], myCRS, edgecolor='black', facecolor='white') # plot the county outline
-xmin, ymin, xmax, ymax = boundary.total_bounds # tells matplotlib to use boundary's total bounds
+xmin, ymin, xmax, ymax = boundary.total_bounds # tells geopandas to use boundary's total bounds
 ax.add_feature(outline_feature) # adds boundary to map
 ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS) # sets map extent to extent of county boundary
 
@@ -54,24 +55,26 @@ for i, name in enumerate(list_lc):
                           edgecolor='black', # plot outline in black
                           facecolor=lc_colours[i], # plot the land covers in their unique colours assigned above
                           linewidth=0, # don't put outlines on land cover polygons
-                          alpha=0.75) # colour saturation
+                          alpha=1) # colour saturation
     ax.add_feature(lc_feat) # plot the above on the axis
 
 flood_5m_feat = ShapelyFeature(flood_5m['geometry'], myCRS, # create feature object of a 5m flood event of Lower Lough Erne
                             edgecolor='black', # plot outline in black
                             facecolor = 'red', # plot the flood in red
                             hatch = 'xx', # with a hatch to distinguish from land cover
-                            linewidth=0.5) # width of bounding lines
+                            linewidth=0.5, # width of bounding lines
+                            alpha = 0.75)
 ax.add_feature(flood_5m_feat) # plot the above on the axis
 
 l_erne_feat = ShapelyFeature(l_erne['geometry'], myCRS, # create a feature object of Lower Lough Erne
                             edgecolor='royalblue', # plot outline in royal blue
                             facecolor = 'royalblue',  # plot Lough Erne in royal blue
-                            linewidth=0.2) # widh of bounding lines
+                            linewidth=0.2, # width of bounding lines
+                            alpha = 0.75)
 ax.add_feature(l_erne_feat) # plot the above on the axis
 
 plt.savefig('Plot.png') # save the plotted figure
-
+"""
 
 
 area_flooded_5m = sum((flood_5m.area)-(sum(l_erne.area))) # the area of land that will be flooded in Lower Lough Erne experiences a 5m flood
